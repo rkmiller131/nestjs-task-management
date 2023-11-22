4eddb3ccf7c37bad02c3a4e98c0c5d095178d584
Rachel Miller <rkmiller131@gmail.com>
2023-11-21 19:36:17 -0800

    Temp add of sql scratch paper

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 
 // Makes this class a singleton that can be injected into other classes (singleton is a design pattern that restricts the instantiation of a class to one object)
+// services in nest are like models in node - they receive information from the controller to talk to the db or external api
 @Injectable()
 export class TasksService {}
