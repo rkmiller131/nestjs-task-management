c5e5b9f3fb4653d3865c98ee66d2c142dfd31877
Rachel Miller <rkmiller131@gmail.com>
2023-11-28 13:08:57 -0800

    Implemented local array for tasks without a dto

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,5 +1,6 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Body, Get, Post } from '@nestjs/common';
 import { TasksService } from './tasks.service'; // injected dependency
+import { Task } from './task.model';
 
 // for '/tasks' route, this controller will handle all requests with its defined methods
 @Controller('tasks')
@@ -9,4 +10,22 @@ export class TasksController {
   // of having to declare the variable within the constructor like this.tasksService = tasksService
   // So with 'private' you're basically saying this tasksService property is a property of the TasksController class
   constructor(private tasksService: TasksService) {}
+
+  // all GET req to /tasks are tied to this method of getAllTasks on the controller, which calls the service
+  @Get()
+  getAllTasks(): Task[] {
+    return this.tasksService.getAllTasks();
+  }
+
+  // You can extract the entire request body with @Body body (assigning the whole body onto the var, 'body')
+  // but sometimes you only want to extract certain parameters from the body such as title and desc, which
+  // guards against other random fields possibly showing up that were sent from the client.
+  @Post()
+  // instead extract only title and description from the body by specifying the keys upon invocation of decorator
+  createTask(
+    @Body('title') title: string,
+    @Body('description') description: string,
+  ): Task {
+    return this.tasksService.createTask(title, description);
+  }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,6 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { Task, TaskStatus } from './task.model';
+import { v4 as uuid } from 'uuid';
 
 // Makes this class a singleton that can be injected into other classes (singleton is a design pattern that restricts the instantiation of a class to one object)
 // services in nest are like models in node - they receive information from the controller to talk to the db or external api
 @Injectable()
-export class TasksService {}
+export class TasksService {
+  // temporarily using local memory to store tasks; will update to postgres later
+  private tasks: Task[] = [];
+
+  // with ts: the result of getAllTasks is an array of Tasks that are in the shape of our Task Model
+  getAllTasks(): Task[] {
+    return this.tasks;
+  }
+
+  createTask(title: string, description: string): Task {
+    const task: Task = {
+      // using the npm package uuid for generating random id strings
+      id: uuid(),
+      title,
+      description,
+      status: TaskStatus.OPEN,
+    };
+    this.tasks.push(task);
+    return task;
+  }
+}
